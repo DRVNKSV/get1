45c3ab2dff7e907b665ec8bb71bc6526e0cd896b
DRVNKSV <serafimaderevyankina@yandex.com>
2026-02-19 19:49:31 +0300

    vdfv

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -1,23 +1,27 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-dac_bits = [0, 1, 2, 3, 4, 5, 6, 7]
+dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
 GPIO.setup(dac_bits, GPIO.OUT)
-dynamic_range = 3.3
+dynamic_range = 3.31.2\
 def voltage_to_number(voltage):
     if not (0.0 <= voltage <= dynamic_range):
         print(f"Out of {dynamic_range:.2f} B")
         print("select 0.0 B")
         return 0
     return int(voltage / dynamic_range *255)
+
 def number_to_dac(number):
-    decimal_mass = [int(element) for elements in bin(number)[2:].zfill(8)]
+    decimal_mass = [int(element) for element in bin(number)[2:].zfill(8)]
     GPIO.output(dac_bits, decimal_mass)
+    print(decimal_mass)
+
 try:
     while True:
         try:
             voltage = float(input("Paste voltage in volts: "))
             number = voltage_to_number(voltage)
+            print(number)
             number_to_dac(number)
 
         except ValueError:
---
+++ get-dac/r2r_dac.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+    
+    def set_number(self, number):
+        
+
+
+    
+
+
+
